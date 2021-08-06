1d65fdfd534537f4dadc13aca73cb3b34927b394
may9noy <may9noy@gmail.com>
2021-08-06 22:37:38 +0900

    Initial commit

---
+++ src/main/java/com/example/jenkins_final_version/JenkinsFinalVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_final_version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFinalVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFinalVersionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/jenkins_final_version/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_final_version;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsFinalVersionApplication.class);
+	}
+
+}
